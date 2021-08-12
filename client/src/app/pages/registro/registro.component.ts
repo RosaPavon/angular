4740c7528f3b2041cd2d2309510802a37a5bbf0e
Rosa Pavón <mrosapavon@gmail.com>
2021-08-12 12:32:07 +0200

    login + registro

--- client/src/app/pages/registro/registro.component.ts
+++ client/src/app/pages/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-registro',
@@ -6,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
+ 
+  usuario:UsuarioModel
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.usuario=new UsuarioModel()
+
+  }
+
+  onSubmit( form: NgForm){
+
+if (form.invalid){return;}
+
+    console.log('Formulario enviado')
+    console.log (this.usuario)
+    console.log(form)
+  }
 
 
 }
